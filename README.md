2b3a3f654246c39463bebc9880d8c76d8733bf4d
Chr1018 <chandan18hr@gmail.com>
2022-06-08 18:49:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-r3q6tr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-r3q6tr)
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <div className="parentDiv">
+        <div className="leftChild">Chr </div>
+        <div className="rigthChild">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          condimentum, magna eu vulputate ultricies, neque nulla mollis velit,
+          nec convallis felis risus nec orci. Morbi lobortis a metus vestibulum
+          blandit. Fusce non sem non lectus semper varius. Vestibulum et ligula
+          eros. Nulla a euismod felis. Phasellus quis tellus maximus tortor
+          pharetra pretium. Aliquam id faucibus nibh, tincidunt gravida tellus.
+          Nullam et velit molestie arcu accumsan porta eget vel odio. Quisque
+          maximus, dolor sit amet posuere condimentum, quam urna vehicula
+          sapien, ut scelerisque diam tortor a enim. Praesent venenatis velit id
+          quam pellentesque, sit amet dapibus est sagittis. Nunc quis ipsum eu
+          mi scelerisque faucibus. Ut varius orci tellus, bibendum viverra
+          tellus efficitur nec. Aenean sagittis ipsum dolor, ut eleifend purus
+          porta eget. Sed nec interdum enim. Sed faucibus justo ipsum, at
+          pulvinar lorem ullamcorper a. Pellentesque dictum massa pulvinar
+          aliquet malesuada. Proin gravida risus at lectus pellentesque euismod.
+          Aliquam semper a odio a convallis. Suspendisse accumsan ultrices
+          velit, sed posuere sapien hendrerit vel. Nunc at malesuada massa, non
+          laoreet arcu. Quisque at posuere enim. Fusce fringilla efficitur odio,
+          vitae convallis quam egestas sed. Vivamus et maximus tortor, at
+          hendrerit magna. Curabitur at pretium turpis, vitae tempor ipsum.
+          Mauris ut dolor ut turpis aliquam mattis. Fusce lacinia finibus odio,
+          in pellentesque massa accumsan vitae. Class aptent taciti sociosqu ad
+          litora torquent per conubia nostra, per inceptos himenaeos. Donec
+          posuere ac odio ac varius. Quisque a metus tincidunt, varius nunc
+          imperdiet, volutpat purus. Sed maximus lorem odio, non gravida eros
+          consectetur vitae. Nullam dapibus lacus non mi molestie ultrices.
+          Integer accumsan arcu id augue facilisis interdum. Mauris mi lectus,
+          pretium a viverra sit amet, maximus a ipsum. Phasellus nec elit eu
+          magna feugiat dapibus. Phasellus sed dictum erat, nec gravida urna.
+          Curabitur at eros quis ante rhoncus malesuada id sit amet mauris.
+          Proin ac orci ut risus tempor tristique. Donec bibendum purus eu
+          ornare congue. Praesent ultrices magna sit amet erat dignissim
+          hendrerit. Donec ut est enim. Vivamus vulputate felis et sollicitudin
+          suscipit. Proin eu volutpat tellus. Vestibulum eget turpis
+          condimentum, efficitur eros vitae, iaculis ex. Nullam bibendum, mauris
+          sit amet auctor fermentum, mi sapien consectetur dui, non lacinia erat
+          arcu at odio. Vestibulum in nunc quis urna placerat condimentum ac sit
+          amet nunc. Suspendisse risus justo, lobortis ut imperdiet eu, iaculis
+          quis lorem. In vitae volutpat leo, ut convallis nibh. Donec sit amet
+          sapien tincidunt, pharetra felis quis, viverra nisl. Aenean eleifend
+          velit felis, vitae posuere felis tristique et. Donec a tempor mi, at
+          molestie diam. Nullam iaculis dignissim odio, sit amet feugiat enim
+          iaculis a. Maecenas a tincidunt turpis. In at tortor sed risus
+          fermentum tincidunt et eu neque. Curabitur a pretium nisl, et ultrices
+          felis. Nulla facilisi. Ut aliquet vitae ipsum id aliquet. Sed at nunc
+          et nisl aliquam suscipit. Mauris vitae eros odio.
+        </div>
+      </div>
+    </div>
+  );
+}
